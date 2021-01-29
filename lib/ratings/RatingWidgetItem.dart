cd963ff48b68262e8cca9f4fe56fb32aa066d980
dvpermyakov <dvpermyakov1@gmail.com>
2021-01-29 13:25:55 +0400

    update ui of RatingWidgetItem.dart

--- lib/ratings/RatingWidgetItem.dart
+++ lib/ratings/RatingWidgetItem.dart
@@ -8,7 +8,46 @@ class RatingWidgetItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(user.name);
+    return Card(
+        color: Colors.white,
+        child: Container(
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+            child: Row(children: [
+              _getImage(context),
+              Expanded(
+                  child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Text(user.name,
+                          maxLines: 3,
+                          style: TextStyle(fontSize: 18),
+                          overflow: TextOverflow.ellipsis)))
+            ])));
   }
 
+  Widget _getImage(BuildContext context) {
+    Color _getColor(BuildContext context) {
+      return Theme.of(context).primaryColor;
+    }
+
+    Widget _getImage() {
+      return Container(
+          width: 64,
+          height: 64,
+          child: Image.network(user.image, fit: BoxFit.cover));
+    }
+
+    Widget _getIcon() {
+      return Container(
+        decoration: ShapeDecoration(
+            shape: CircleBorder(
+                side: BorderSide(width: 4, color: _getColor(context)))),
+        width: 64,
+        height: 64,
+        child: Icon(Icons.person, size: 32, color: _getColor(context)),
+      );
+    }
+
+    final child = user.image != null ? _getImage() : _getIcon();
+    return ClipRRect(borderRadius: BorderRadius.circular(32), child: child);
+  }
 }
